51e320cd6f80adb21c8d73bc866ba7939144b0e0
Khalid Elshafie <abolkog@gmail.com>
2020-05-26 12:38:28 +1000

    Added language and courses

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +10,20 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+  // Seed the languages tables if no language exists
+  try {
+    const count = await strapi.query("language").count({});
+    if (count === 0) {
+      const seed = require("../../data/seed/languages.json");
+      seed.forEach((language) => {
+        strapi.services.language.create({
+          name: language.name,
+          iso2: language.iso2,
+        });
+      });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
