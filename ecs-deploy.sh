e3395ffd043f53ac7b17a1643bc46647fe2cc60b
Khalid Elshafie <abolkog@gmail.com>
2020-07-12 20:02:49 +1000

    Update ecs cluster name

--- ecs-deploy.sh
+++ ecs-deploy.sh
@@ -5,7 +5,7 @@ set -e
 ENV=$1
 ECR_IMAGE="394363841155.dkr.ecr.eu-central-1.amazonaws.com/nyala-edu"
 TASK_FAMILY="edu-${ENV}-td"
-ECS_CLUSTER="edu-${ENV}"
+ECS_CLUSTER="nyala-${ENV}"
 SERVICE_NAME="edu-${ENV}-service"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "$AWS_DEFAULT_REGION")
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
