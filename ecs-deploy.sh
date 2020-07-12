1393b6981704ae188683c307253728ddccada6df
Khalid Elshafie <abolkog@gmail.com>
2020-07-12 19:47:00 +1000

    Update deployment settings

--- ecs-deploy.sh
+++ ecs-deploy.sh
@@ -4,9 +4,9 @@
 set -e
 ENV=$1
 ECR_IMAGE="394363841155.dkr.ecr.eu-central-1.amazonaws.com/nyala-edu"
-TASK_FAMILY="edu-dev-fargate-td"
-ECS_CLUSTER="nyala-edu-fargate"
-SERVICE_NAME="nyala-edu-fargate-service"
+TASK_FAMILY="edu-${ENV}-td"
+ECS_CLUSTER="edu-${ENV}"
+SERVICE_NAME="edu-${ENV}-service"
 TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "$AWS_DEFAULT_REGION")
 NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities)')
 NEW_TASK_INFO=$(aws ecs register-task-definition --region "$AWS_DEFAULT_REGION" --cli-input-json "$NEW_TASK_DEFINTIION")
