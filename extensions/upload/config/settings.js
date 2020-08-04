aed1fd5b7bc2648de52464cca046e0cbafc58c64
Viktoriia <kosenchiha@gmail.com>
2020-08-04 08:31:58 +0100

    Add profile collection to strapi (#6)

---
+++ extensions/upload/config/settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+  provider: 'aws-s3',
+  providerOptions: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+    region: process.env.AWS_REGION || 'eu-central-1',
+    params: {
+      Bucket: 'edu-profile-pictures',
+    },
+  },
+};
