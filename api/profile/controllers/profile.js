b839403025023057fa620ea966a469a834d01c1c
Alaa Yahia <alaay873@live.com>
2020-08-07 17:03:38 +0200

    Add profile actions

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -1,8 +1,35 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async find(ctx) {
+    const { user } = ctx.state;
+    if (user) {
+      if (user.profile) {
+        const entity = await strapi.services.profile.findOne({ id: user.profile });
+        return sanitizeEntity(entity, { model: strapi.models.profile });
 
-module.exports = {};
+      }
+      else {
+        return {}
+      }
+    }
+    else {
+      //return ctx.response.badRequest(`Course slug must be unique ${slug}`);
+      return ctx.unauthorized(`You can't see me`);
+    }
+  },
+  async update(ctx) {
+    const user = ctx.state.user;
+    const body = ctx.request.body;
+    const profile = { ...body, user:user.id };
+    let entity;
+    if (user.profile) {
+      entity = await strapi.services.profile.update({id:user.profile}, profile);
+    }
+    else {
+      entity = await strapi.services.profile.create(profile);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.profile });
+  },
+};
