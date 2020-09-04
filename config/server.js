f815c06d674fe0b85e9f46787098275d4a31c41b
Khalid Elshafie <abolkog@gmail.com>
2020-09-04 22:37:10 +1000

    Update env variables

--- config/server.js
+++ config/server.js
@@ -1,21 +1,13 @@
 module.exports = ({ env }) => {
-  const isDev = env('NODE_ENV') === 'development';
-
-  const host = env('HOST', '0.0.0.0');
-  const port = env.int('PORT', 1437);
-
-  const APP_URL = env('APP_URL', 'https://edu-api.nyaladev.com');
-  const url = isDev ? `http:${host}:${port}` : APP_URL;
-
   return {
-    host,
-    port,
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1437),
     cron: {
       enabled: false,
     },
     admin: {
       autoOpen: false,
     },
-    // url,
+    url: env('APP_URL', 'https://edu-api-prod.herokuapp.com'),
   };
 };
