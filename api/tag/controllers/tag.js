59396b1b27f089fd4c03ed17eba05f0191cc38b0
Khalid Elshafie <abolkog@gmail.com>
2020-10-23 12:33:33 +1100

    Add instructor dashboard support

--- api/tag/controllers/tag.js
+++ api/tag/controllers/tag.js
@@ -3,13 +3,24 @@ const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
   async find(ctx) {
-    const tags = await strapi.query('tag').find(ctx.query, ['courses', 'courses.lectures']);
-
+    const tags = await strapi
+      .query('tag')
+      .find(ctx.query, [
+        { courses: (qb) => qb.where('status', 'Published') },
+        'courses.lectures',
+        'courses.language',
+      ]);
     return tags.map((tag) => sanitizeEntity(tag, { model: strapi.models.tag }));
   },
   async findOne(ctx) {
     const { id } = ctx.params;
-    const tag = await strapi.query('tag').findOne({ id }, ['courses', 'courses.lectures']);
+    const tag = await strapi
+      .query('tag')
+      .findOne({ id }, [
+        { courses: (qb) => qb.where('status', 'Published') },
+        'courses.lectures',
+        'courses.language',
+      ]);
 
     return sanitizeEntity(tag, { model: strapi.models.tag });
   },
--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,18 +1,9 @@
 'use strict';
 const { languageSeeder, courseSeeder } = require('./seeder');
+const { initPermissions } = require('./permissions.helper');
 
 module.exports = async () => {
-  // Seed the languages tables if no language exists
   languageSeeder();
-
-  // Seed the courses and lectures table
   courseSeeder();
-
-  // User seeder ?
-  // try {
-  //   const users = await strapi.plugins['users-permissions'].services.user.fetchAll();
-  //   console.log(users);
-  // } catch (e) {
-  //   console.error('Failed badly');
-  // }
+  initPermissions();
 };
