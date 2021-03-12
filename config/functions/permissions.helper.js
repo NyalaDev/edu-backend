4151b6604791678986ba6a0e4757943818ce767d
mozafar <kabaros@gmail.com>
2021-03-12 10:07:20 +0000

    Add mailchimp script

--- config/functions/permissions.helper.js
+++ config/functions/permissions.helper.js
@@ -56,7 +56,7 @@ const initPermissions = async () => {
 
   await enablePermission('authenticated', 'profile', 'create');
   await enablePermission('authenticated', 'profile', 'update');
-    await enablePermission('authenticated', 'pr', 'create');
+  await enablePermission('authenticated', 'pr', 'create');
 };
 
 module.exports = {
