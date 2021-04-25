0fa006b42e3a90a0cb2cdffcfde82c24c583f150
Alaa Yahia <alaay873@live.com>
2021-04-25 14:22:13 +0200

    Add try catch

--- extensions/users-permissions/models/user.js
+++ extensions/users-permissions/models/user.js
@@ -1,21 +1,24 @@
 'use strict';
 const axios = require('axios');
+const url = process.env.EMAIL_SUBSCRIBE_URL || `https://barmga-lambda.nyaladev.com/email-subscribe`;
 
 const subscribeToMailingList = async (values) => {
-  const { data } = await axios.post(`https://barmga-lambda.nyaladev.com/email-subscribe`, values);
+  const { data } = await axios.post(url, values);
   return data;
 };
 
 module.exports = {
   lifecycles: {
     afterCreate: async (model, response, options) => {
-      if (model.emailSubscription && model.language) {
-        await subscribeToMailingList({
-          email: model.email,
-          LANGUAGE: model.language,
-        }).catch((e) => {
-          console.log(e);
-        });
+      try {
+        if (model.emailSubscription && model.language) {
+          await subscribeToMailingList({
+            email: model.email,
+            LANGUAGE: model.language,
+          });
+        }
+      } catch (error) {
+        strapi.log.error(error);
       }
     },
   },
