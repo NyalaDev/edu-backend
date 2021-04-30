faa037beacd57695e0f9effc7e5051ca0bcc70af
mozafar <mozafar@nyala.dev>
2021-04-30 11:10:06 +0100

    Update docker-compose file postgres port
    Update docker-compose file to change the port for Postgres so that it does not conflict if there is another instance of postgres already installed

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => {
         settings: {
           client: 'postgres',
           host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
+          port: env.int('DATABASE_PORT', 5433),
           database: env('DATABASE_NAME', 'orula'),
           username: env('DATABASE_USERNAME', 'nyala'),
           password: env('DATABASE_PASSWORD', 'password'),
